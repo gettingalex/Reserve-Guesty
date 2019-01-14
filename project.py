0ddbef375e2945fbc4519bba0b0d62cf9f777ffe
Alexandre <ar_creation@hotmail.com>
2019-01-14 16:42:26 -0600

    Add stripe payment on valid booking
    Add stripe payment for both front and back end. Create new merci.html as the confirmation of payment page

--- project.py
+++ project.py
@@ -5,9 +5,15 @@ from flask import (Flask, render_template,
 import httplib2
 import json
 import requests
+import stripe
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+pub_key = 'pk_test_gQRV3GY6b5U8JWDUscP9QKUT'
+secret_key_stripe = 'sk_test_ZVGWXHJMkoTWtQsptmaySnIn'
+
+stripe.api_key = secret_key_stripe
 
 @app.route('/')
 @app.route('/reserver')
@@ -56,6 +62,7 @@ def listingForm():
     for i in listings:
         prices.append(i['price'])
     subTotal = sum(prices)
+    stripeSubTotal = subTotal*100
     avgNight = 0
     if prices != []:
         avgNight = subTotal/len(prices)
@@ -63,7 +70,7 @@ def listingForm():
             flash('Les dates que vous avez selectionnés ne sont pas disponibles.')
         if allFree == True:
             flash(unit)
-            return render_template('booking.html', avgNight=avgNight, subTotal=subTotal)   
+            return render_template('booking.html', avgNight=avgNight, subTotal=subTotal, pub_key=pub_key, stripeSubTotal=stripeSubTotal, fromDate=fromDate, toDate=toDate)   
         if allBooked == False and allFree == False:
 
             flash('Certaines dates sélectionnés ne sont pas disponibles.')
@@ -72,6 +79,25 @@ def listingForm():
     flash(listings)
     return redirect(url_for('checkDate'))
 
+@app.route('/merci')
+def merci():
+    return render_template('merci.html')
+
+@app.route('/pay/<int:stripeSubTotal>/', methods=['POST'])
+def pay (stripeSubTotal):
+    customer = stripe.Customer.create(email=request.form['stripeEmail'], source=request.form['stripeToken'])
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=stripeSubTotal,
+        currency='usd',
+        description='Reservation - Hebergement - Sejour de Reve',
+        receipt_email='ar_creation@me.com'
+        )
+
+    return redirect(url_for('merci'))
+    #for testing and see if the request was sent and return from stripe
+    #print(request.form)
+    #return ''
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
